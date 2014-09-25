4938c9a4f4779149686caad5993131077b1eab4f
alnutile <alfrednutile@gmail.com>
2014-09-24 22:49:45 -0400

    Start base install

---
+++ env.local.php
@@ -0,0 +1,25 @@
+<?php
+
+return array(
+    'BEAN_HOST' => 'localhost',
+    'BEAN_QUEUE' => 'default',
+    'BEAN_QUEUE_BATCHES' => 'default',
+    'BATCHES_QUEUE_LIMIT' => '10',
+    'BEAN_QUEUE_REPORTING_OVERVIEW' => 'bean_queue_reporting_overview',
+    'DB_NAME' => 'behat',
+    'DB_PASSWORD' => 'secret',
+    'DB_USER' => 'root',
+    'DB_URL' => 'localhost',
+    'BEHAT_TEST_FILES_LIMIT' => '2',
+    'DB_DRIVER' => 'mysql',
+    'GIT_TOKEN' => '9c946c15353d92c92d7d928d11e16586762cd5f6',
+    'GIT_USERNAME' => 'alnutile',
+    'DOMAIN' => 'behat_laravel.dev',
+    'PORT' => '80',
+    'MEMORY_LIMIT' => '32',
+    'SQS_KEY'=>'barfoo',
+	'SQS_SECRET'=>'foobar',
+	'SQS_REGION'=>'bar',
+	'SQS_QUEUE_NAME'=>'foo',
+	'RESULTS_QUEUE_LIMIT' => 10
+);
---
+++ readme.md
@@ -0,0 +1,17 @@
+## Base install with style guides in mind
+
+Style Guides are [here](https://github.com/alnutile/team_style_guides)
+
+## Setting up Environment Variables
+
+Copy env.local.php to .env.local.php and set the settings as needed
+
+## Angular
+
+You will see a great working example of ngMock which means NO backend is needed from Laravel. In this case it is just a router to hand off to
+Angular BUT could just be html.
+
+## Gulp
+
+Auto run tests folder
+Auto set Sass file to css
